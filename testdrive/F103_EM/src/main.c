433d8050cf7e28a26bc01c85e62c598e54caae96
Ayu <claire@ayu.land>
2024-01-01 19:43:48 +0800

    Electromagnet test-drive

---
+++ testdrive/F103_EM/src/main.c
@@ -0,0 +1,140 @@
+#include <stm32f1xx_hal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+static uint8_t swv_buf[256];
+static size_t swv_buf_ptr = 0;
+__attribute__ ((noinline, used))
+void swv_trap_line()
+{
+  *(volatile char *)swv_buf;
+}
+static inline void swv_putchar(uint8_t c)
+{
+  // ITM_SendChar(c);
+  if (c == '\n') {
+    swv_buf[swv_buf_ptr >= sizeof swv_buf ?
+      (sizeof swv_buf - 1) : swv_buf_ptr] = '\0';
+    swv_trap_line();
+    swv_buf_ptr = 0;
+  } else if (++swv_buf_ptr <= sizeof swv_buf) {
+    swv_buf[swv_buf_ptr - 1] = c;
+  }
+}
+static void swv_printf(const char *restrict fmt, ...)
+{
+  char s[256];
+  va_list args;
+  va_start(args, fmt);
+  int r = vsnprintf(s, sizeof s, fmt, args);
+  for (int i = 0; i < r && i < sizeof s - 1; i++) swv_putchar(s[i]);
+  if (r >= sizeof s) {
+    for (int i = 0; i < 3; i++) swv_putchar('.');
+    swv_putchar('\n');
+  }
+}
+
+TIM_HandleTypeDef tim2 = { 0 };
+
+int main()
+{
+  HAL_Init();
+
+  // ======== GPIO ========
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  GPIO_InitTypeDef gpio_init;
+
+/*
+  gpio_init.Pin = GPIO_PIN_1;
+  gpio_init.Mode = GPIO_MODE_OUTPUT_PP;
+  gpio_init.Pull = GPIO_PULLUP;
+  gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &gpio_init);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+*/
+
+  // ======== Clocks ========
+  RCC_OscInitTypeDef osc_init = { 0 };
+  osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  osc_init.HSEState = RCC_HSE_ON;
+  osc_init.PLL.PLLState = RCC_PLL_ON;
+  osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  osc_init.PLL.PLLMUL = RCC_PLL_MUL9;
+  HAL_RCC_OscConfig(&osc_init);
+
+  RCC_ClkInitTypeDef clk_init = { 0 };
+  clk_init.ClockType =
+    RCC_CLOCKTYPE_SYSCLK |
+    RCC_CLOCKTYPE_HCLK |
+    RCC_CLOCKTYPE_PCLK1 |
+    RCC_CLOCKTYPE_PCLK2;
+  clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+  clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&clk_init, FLASH_LATENCY_2);
+
+  // Cycle count
+  CoreDebug->DEMCR &= ~CoreDebug_DEMCR_TRCENA_Msk;
+  CoreDebug->DEMCR |=  CoreDebug_DEMCR_TRCENA_Msk;
+  DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk;
+  DWT->CTRL |=  DWT_CTRL_CYCCNTENA_Msk;
+  DWT->CYCCNT = 0;
+
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
+  // ======== Timer ========
+  // TIM2_CH2 (PA1)
+  gpio_init.Pin = GPIO_PIN_1;
+  gpio_init.Mode = GPIO_MODE_AF_PP;
+  gpio_init.Pull = GPIO_NOPULL;
+  gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &gpio_init);
+
+  // 72 MHz clock, 7.2 kHz PWM, resolution ~10 bits (max. 1000)
+  tim2 = (TIM_HandleTypeDef){
+    .Instance = TIM2,
+    .Init = {
+      .Prescaler = 1 - 1,
+      .CounterMode = TIM_COUNTERMODE_UP,
+      .Period = 10000 - 1,
+      .ClockDivision = TIM_CLOCKDIVISION_DIV1,
+      .RepetitionCounter = 0,
+    },
+  };
+  TIM_OC_InitTypeDef tim2_ch2_oc_init = {
+    .OCMode = TIM_OCMODE_PWM1,
+    .Pulse = 100, // to be filled
+    .OCPolarity = TIM_OCPOLARITY_HIGH,
+  };
+  __HAL_RCC_TIM2_CLK_ENABLE();
+  HAL_TIM_PWM_Init(&tim2);
+  HAL_TIM_PWM_ConfigChannel(&tim2, &tim2_ch2_oc_init, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&tim2, TIM_CHANNEL_2);
+  while (1) {
+    for (int i = 0; i < 100; i++) {
+      TIM2->CCR2 = i * 10;
+      HAL_Delay(10);
+    }
+  }
+
+/*
+  while (1) {
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+    HAL_Delay(1);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+    HAL_Delay(99);
+  }
+*/
+  while (1) { }
+}
+
+void SysTick_Handler()
+{
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+}
