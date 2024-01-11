66b3e4d764969da97370215794d3454d6d7dbb2f
Ayu <claire@ayu.land>
2024-01-11 15:42:48 +0800

    Blinking LED on unit hardware

---
+++ flowerbed/fw_unit/src/main.c
@@ -0,0 +1,106 @@
+#include <stm32g0xx_hal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#define ACT_LED_PIN   GPIO_PIN_15
+#define ACT_LED_PORT  GPIOC
+
+// #define RELEASE
+#ifndef RELEASE
+static uint8_t swv_buf[256];
+static size_t swv_buf_ptr = 0;
+__attribute__ ((noinline, used))
+void swv_trap_line()
+{
+  *(volatile char *)swv_buf;
+}
+static inline void swv_putchar(uint8_t c)
+{
+  // ITM_SendChar(c);
+  if (c == '\n') {
+    swv_buf[swv_buf_ptr >= sizeof swv_buf ?
+      (sizeof swv_buf - 1) : swv_buf_ptr] = '\0';
+    swv_trap_line();
+    swv_buf_ptr = 0;
+  } else if (++swv_buf_ptr <= sizeof swv_buf) {
+    swv_buf[swv_buf_ptr - 1] = c;
+  }
+}
+static void swv_printf(const char *restrict fmt, ...)
+{
+  char s[256];
+  va_list args;
+  va_start(args, fmt);
+  int r = vsnprintf(s, sizeof s, fmt, args);
+  for (int i = 0; i < r && i < sizeof s - 1; i++) swv_putchar(s[i]);
+  if (r >= sizeof s) {
+    for (int i = 0; i < 3; i++) swv_putchar('.');
+    swv_putchar('\n');
+  }
+}
+#else
+#define swv_printf(...)
+#endif
+
+int main()
+{
+  HAL_Init();
+
+  // ======== GPIO ========
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  GPIO_InitTypeDef gpio_init;
+
+  // Act LED
+  gpio_init = (GPIO_InitTypeDef){
+    .Pin = ACT_LED_PIN,
+    .Mode = GPIO_MODE_OUTPUT_PP,
+    .Pull = GPIO_NOPULL,
+    .Speed = GPIO_SPEED_FREQ_HIGH,
+  };
+  HAL_GPIO_Init(ACT_LED_PORT, &gpio_init);
+  HAL_GPIO_WritePin(ACT_LED_PORT, ACT_LED_PIN, 0);
+
+  // SWD (PA13, PA14)
+  gpio_init.Pin = GPIO_PIN_13 | GPIO_PIN_14;
+  gpio_init.Mode = GPIO_MODE_AF_PP; // Pass over control to AF peripheral
+  gpio_init.Alternate = GPIO_AF0_SWJ;
+  gpio_init.Pull = GPIO_PULLUP;
+  gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &gpio_init);
+
+  // ======== Clocks ========
+  RCC_OscInitTypeDef osc_init = { 0 };
+  osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  osc_init.HSIState = RCC_HSI_ON;
+  osc_init.PLL.PLLState = RCC_PLL_OFF;
+  HAL_RCC_OscConfig(&osc_init);
+
+  RCC_ClkInitTypeDef clk_init = { 0 };
+  clk_init.ClockType =
+    RCC_CLOCKTYPE_SYSCLK |
+    RCC_CLOCKTYPE_HCLK |
+    RCC_CLOCKTYPE_PCLK1;
+  clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSI; // 16 MHz
+  clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clk_init.APB1CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&clk_init, FLASH_LATENCY_2);
+
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
+  while (1) {
+    HAL_GPIO_WritePin(ACT_LED_PORT, ACT_LED_PIN, 1);
+    HAL_Delay(500);
+    HAL_GPIO_WritePin(ACT_LED_PORT, ACT_LED_PIN, 0);
+    HAL_Delay(500);
+  }
+}
+
+void SysTick_Handler()
+{
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+}
